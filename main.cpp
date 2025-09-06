042719f5372fd0b6356c618f9078173b26844d56
Noah <noahgauche321@gmail.com>
2025-09-06 23:31:33 +0200

    Change the auto the the explicit type

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    auto app = Gtk::Application::create("MC Server Manager");
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("MC Server Manager");
 
     // Shows the window and returns when it is closed.
     return app->make_window_and_run<MCSM::MCServerManagerWindow>(argc, argv);
