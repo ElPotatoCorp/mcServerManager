7658f22c6ef9719707a1669337cf5c48b1eeaf55
Noah <noahgauche321@gmail.com>
2025-09-10 10:20:18 +0200

    Added a few constants

---
+++ mCServerManager/Utils/Constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const char *START_SCRIPT_NAME_PROPERTY = "start-script-name";
+const char *WORLD_NAME_PROPERTY        = "level-name";
+const char *DESCRIPTION_PROPERTY       = "motd";
+const char *PORT_PROPERTY              = "server-port";
+const char *MAX_PLAYERS_PROPERTY       = "max-players";
+const char *VIEW_DISTANCE_PROPERTY     = "view-distance";
+const char *GAMEMODE_PROPERTY          = "gamemode";
+const char *DIFFICULTY_PROPERTY        = "difficulty";
+const char *HARDCORE_PROPERTY          = "hardcore";
+const char *PVP_PROPERTY               = "pvp";
+const char *FLY_PROPERTY               = "allow-flight";
+const char *NETHER_PROPERTY            = "allow-nether";
+const char *WHITELIST_PROPERTY         = "white-list";
+
+#endif // CONSTANTS_H
